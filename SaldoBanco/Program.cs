33c8efafd36b655031e1b421f6c23d352f07c756
Joao Paulo Lazarin <jplazarin@gmail.com>
2022-05-30 11:29:41 -0300

    Add project files.

---
+++ SaldoBanco/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SaldoBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            saldoBanco c = new saldoBanco();
+
+            Console.Write("Entre numero da conta:");
+            c.numeroConta = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            Console.Write("Entre o titular da conta:");
+            //Console.WriteLine();
+            c.nomeUsuario = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n)?");
+
+            string depositoInicial = Console.ReadLine();
+
+            if (depositoInicial == "s")
+            {
+                Console.Write("Entre o valor de deposito inicial: ");
+                c.valorDeposito = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para deposito: ");
+            int valorMovimento = int.Parse(Console.ReadLine());
+            c.AdicionarDeposito(valorMovimento);
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para saque: ");
+            valorMovimento = int.Parse(Console.ReadLine());
+            c.RemoverValor(valorMovimento);
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ SaldoBanco/saldoBanco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaldoBanco
+{
+    class saldoBanco
+    {
+        public int numeroConta;
+        public string nomeUsuario;
+        public double valorDeposito;
+        
+        public void AdicionarDeposito(int valordep)
+        {
+            valorDeposito += valordep;
+        }
+        public void RemoverValor(int quantidade)
+        {
+            valorDeposito -= quantidade;
+            valorDeposito -= 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta: "
+                + " Conta JP " + numeroConta
+                + " Titular: " + nomeUsuario.ToString() + " , "
+                 + " Saldo: $ "
+                + valorDeposito.ToString("F2");
+          //  "Conta " + c.numeroConta + ", Titular: " + c.nomeUsuario + ", Saldo: $ " + c.valorDeposito
+        }
+    }
+}
